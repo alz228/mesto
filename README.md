350b26a3c3db0a2bd4b88276e118035f5f6c69f1
Alexandr <zaru4evsky@yandex.ru>
2022-03-03 12:12:44 +0300

    fixed.03

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 создание сайта с реализованной функцией попап, редактирующий информацию.
 
+при помощи CSS   HTML   JS
+
 ## Проект сделат по макету
 
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+
 ### ссылка на Github pages
 https://alz228.github.io/mesto/
 
--- script.js
+++ script.js
@@ -1,91 +1,29 @@
-let mainContainer = document.querySelector('.maincontainer');
-
-let formButton = document.querySelector('.form__button-save');
-
-let profileContainer = document.querySelector('.profile__info');
-
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-
-let formSubmit = document.querySelector('.form'); // заменить по бэму
-
+let formSubmit = document.querySelector('.edit-form');
 let profileInfoButton = document.querySelector('.profile__info-button');
-let popupMain  = document.querySelector('.popup');
+let popupMain = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
+let name = document.querySelector('.edit-form__input_name');
+let description = document.querySelector('.edit-form__input_description');
 
-let likeButton = document.querySelector('.element__like');
-
-
-
-function popupOpenClose(){
-   
-console.log('opening');
-popupMain.classList.toggle('popup__hide');
-
-   
+function popupOpen(){
+popupMain.classList.remove('popup_hide')
+ name.value = profileName.textContent;
+description.value = profileDescription.textContent;
 }
 
+function popupClose() {
+   popupMain.classList.add('popup_hide');
+}
 
-profileInfoButton.addEventListener('click', popupOpenClose);
-
-
- function changeName(evt){
+function changeName(evt) {
    evt.preventDefault();
-
-
-   let name = document.querySelector('.form__name');
-   let description = document.querySelector('.form__description');
-   
-profileName.textContent= name.value;
-profileDescription.textContent= description.value;
-
-
-popupOpenClose();
- }
-
- formSubmit.addEventListener('submit', changeName);
- 
- closeButton.addEventListener('click', popupOpenClose);
-
-/*let form = document.querySelector('.edit__form');
-// let formName = edit__form.querySelector('.form__name');
-// let formDescription = edit__form.querySelector('.form__description');
-
-let profileInfo = document.querySelector('.profile__info');
-
-
-
-let formButton = document.querySelector('.form__button');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-
-
-let a='';
-let b='';
-
-
-function addInfo() {
-    let authorName = document.querySelector('.form__name');
-    let authorDescription = document.querySelector('.form__description');
-
-    console.log(authorName.value);
-    console.log(authorDescription.value);
-
-    profileInfo.insertAdjacentHTML('beforeend', `
-    <div class="song">
-   <h4 class="song__artist">${authorName.value}</h4>
-  <p class="song__title">${authorDescription.value}</p>
-     <button class="song__like"></button>
-    </div>
- `);
+   profileName.textContent = name.value;
+   profileDescription.textContent = description.value;
+   popupClose();
 }
 
-formButton.addEventListener('click', addInfo);
-
-// formButton.onclick = function(){
-//     let authorName = document.querySelector('.form__name');
-//     let authorDescription = document.querySelector('.form__description');
-
-//     console.log(authorName.value);
-//     console.log(authorDescription.value);
-// }*/
+profileInfoButton.addEventListener('click', popupOpen);
+formSubmit.addEventListener('submit', changeName);
+closeButton.addEventListener('click', popupClose);
